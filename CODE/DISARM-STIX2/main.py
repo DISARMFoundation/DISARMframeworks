8937d50328b57e76448ec4785ae6d838ab0e3386
VVX7 <info@vvx7.io>
2022-07-02 00:06:46 -0400

    add subtechniques to stix2 generator

--- CODE/DISARM-STIX2/main.py
+++ CODE/DISARM-STIX2/main.py
@@ -11,7 +11,7 @@ from stix2 import (Bundle, AttackPattern, ThreatActor, IntrusionSet, Relationshi
 from stix2.properties import (ReferenceProperty, ListProperty, StringProperty, TimestampProperty, BooleanProperty, IntegerProperty)
 
 import helpers
-from objects import tactic, technique, matrix, bundle
+from objects import tactic, technique, matrix, bundle, relationship
 from helpers import xlsx, file
 
 
@@ -25,24 +25,24 @@ def generate_disarm_stix():
 
     tactics = tactic.make_disarm_tactics(data)
     techniques = technique.make_disarm_techniques(data)
+    subtechniques = technique.make_disarm_subtechniques(data)
+    subtechnique_relationships = relationship.make_disarm_subtechnique_relationships(techniques, subtechniques)
     navigator_matrix = matrix.make_disarm_matrix(tactics)
 
     stix_objects = []
     stix_objects.append(tactics)
     stix_objects.append(techniques)
+    stix_objects.append(subtechniques)
+    stix_objects.append(subtechnique_relationships)
     stix_objects.append(navigator_matrix)
 
     stix_objects = [item for sublist in stix_objects for item in sublist]
-
-    # for i in stix_objects:
-    #     print(i)
 
     disarm_bundle = bundle.make_stix_bundle(stix_objects)
 
     helpers.file.clean_output_dir()
     helpers.file.write_files(stix_objects)
     helpers.file.write_bundle(disarm_bundle, "DISARM")
-
 
 
 if __name__ == "__main__":
--- CODE/DISARM-STIX2/objects/relationship.py
+++ CODE/DISARM-STIX2/objects/relationship.py
@@ -19,3 +19,27 @@ def make_disarm_subtechnique_relationship(source, target):
     )
 
     return relationship
+
+
+def make_disarm_subtechnique_relationships(techniques, subtechniques):
+    """Creates a map of technique and sub-technique.
+
+    Args:
+        techniques (list): List of STIX2 technique objects.
+        subtechniques (list): List of STIX2 subtechnique objects.
+
+    Returns:
+        A Relationship object.
+
+    """
+    technique_ids = {}
+    for technique in techniques:
+        technique_ids[technique["external_references"][0]["external_id"]] = technique["id"]
+
+    relationships = []
+    for subtechnique in subtechniques:
+        technique_id = technique_ids[subtechnique["external_references"][0]["external_id"].split(".")[0]]
+        relationship = make_disarm_subtechnique_relationship(subtechnique["id"], technique_id)
+        relationships.append(relationship)
+
+    return relationships
